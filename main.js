0f58ee69d3433a711c3ded24e103ec1a89252a30
Antônio Miguel Batista Teixeira <antonio.miguel@estudante.ufjf.br>
2025-08-16 00:14:33 -0300

    interação dos cliques e finalização

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
-import { getTabuleiro } from "/restaUm.js";
+import { getTabuleiro, selecionar, getPecaSelecionada } from "./restaUm.js";
 
 const campoJogo = document.getElementById("tabuleiro-container");
 
+function clicouCelula(clique) {
+  const l = parseInt(clique.target.dataset.linha);
+  const c = parseInt(clique.target.dataset.coluna);
+
+  selecionar(l, c);
+  desenhaTudo();
+}
+
 function desenhaTudo() {
   const jogo = getTabuleiro();
+  const pecaAtiva = getPecaSelecionada();
   campoJogo.innerHTML = "";
 
   jogo.forEach((linha, i) => {
@@ -19,12 +28,20 @@ function desenhaTudo() {
         cel.classList.add("invalido");
       }
 
+      if (pecaAtiva && pecaAtiva.linha === i && pecaAtiva.coluna === j) {
+        cel.classList.add("selecionada");
+      }
+
       cel.dataset.linha = i;
       cel.dataset.coluna = j;
+
+      if (celula !== -1) {
+        cel.addEventListener("click", clicouCelula);
+      }
 
       campoJogo.appendChild(cel);
     });
   });
 }
 
-desenhaTudo();
+desenhaTudo();  
